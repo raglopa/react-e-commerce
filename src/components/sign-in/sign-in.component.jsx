27c59b71314b1d2f22b302858061ffcbcb76d483
Attila Polgar <raglopa@gmail.com>
2019-08-10 16:39:08 +0200

    feat(auth): integrate firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import "./sign-in.styles.scss"
 import FormInput from "../form-input/form-input.component"
 import CustomButton from "../custom-button/custom-button.component"
+import { signInWithGoogle } from "../../firebase/firebase.utils"
 
 class SignIn extends React.Component {
   state = {
@@ -43,7 +44,12 @@ class SignIn extends React.Component {
             required
             label="password"
           />
-          <CustomButton type="submit">Sign in</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign in</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with google
+            </CustomButton>
+          </div>
         </form>
       </div>
     )
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyBmR2dNZ04BuUCiS7p5sadpbCmBB2sSBXs",
+  authDomain: "react-e-commerce-6faa3.firebaseapp.com",
+  databaseURL: "https://react-e-commerce-6faa3.firebaseio.com",
+  projectId: "react-e-commerce-6faa3",
+  storageBucket: "",
+  messagingSenderId: "825195664658",
+  appId: "1:825195664658:web:41392d6e3413f591"
+}
